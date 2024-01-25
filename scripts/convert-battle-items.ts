038de552cb872d5138755c8ebb562364ccff0013
malaow3 <malaow3@yahoo.com>
2024-01-24 22:32:35 -0500

    Update scripts

--- scripts/convert-battle-items.ts
+++
@@ -1,8 +0,0 @@
-/// Convert the battle items from items.js to a JSON file
-
-import { writeFileSync } from "fs";
-import { BattleItems } from "./items";
-
-// Write the JSON
-const json = JSON.stringify(BattleItems, null, 2);
-writeFileSync("../battleItems.json", json);
