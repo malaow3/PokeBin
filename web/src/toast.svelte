bd1fc2ee98fbe2a8d2f79637b7b873b38625daa9
malaow3 <malaow3@yahoo.com>
2024-03-02 19:25:27 +0000

    Add copy paste button

---
+++ web/src/toast.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { fade } from "svelte/transition";
+	export let message = "";
+	export let show = false;
+
+	// Logic to automatically hide the toast after a duration
+	$: if (show) {
+		setTimeout(() => {
+			show = false;
+		}, 3000); // Adjust the time as needed
+	}
+</script>
+
+{#if show}
+	<div
+		transition:fade={{ duration: 300 }}
+		class="fixed top-4 right-4 p-2 bg-blue-500 text-white font-black rounded-lg shadow-lg"
+	>
+		{message}
+	</div>
+{/if}
